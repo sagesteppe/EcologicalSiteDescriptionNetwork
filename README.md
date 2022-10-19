f39333a038f0094a104f62175974897b663f0bc8
sagesteppe <reedbenkendorf27@gmail.com>
2022-10-19 16:43:50 -0600

    added readme working on controlling cols

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # EcologicalSiteDescriptionNetwork
+
+An Ecological Site Description is used to by Land Management Agencies to group similar areas, or motifs, across a landscape. In theory these ecological sites, due to their similar biophysical, and climatic properties have similar soils and hence vegetation, and should respond in similar manners to land management. A tradeoff exists between producing many narrow descriptions, and several broader descriptions. 
+
+In general the landscapes with ESD's completed are *somewhat* homogenous. However, a number of rare sites are still present. I argue that we can form, and as *needed* utilize broader conceptions of ESD's to supply context for rare sites across the landscape.
+
+Here we utilize an Assess, Inventory, and Monitor (AIM) sample design, which had ESD verification occur at 130 plots to serve as a focal group of ESD's. From this focal group we branch outwards along the ESD documents 'similar and associated sites' (this name... my own) sections to develop a framework for relating the ESD's using simple binary and undirected associations. 
--- scripts/parseFocalESDs.R
+++ scripts/parseFocalESDs.R
@@ -111,15 +111,34 @@ V(graph)$degree <- degree(graph)
 communityMulti <- multilevel.community(graph)
 V(graph)$color <- membership(communityMulti) # but need sync up. 
 
+V(graph)$color
 lay <- layout_with_fr(graph)
 
 par(bg = 'black')
 plot(graph, vertex.label = NA,
      mark.groups = communityMulti,
+    # mark.col = c('royalblue1', 'pink3', 'orange4',
+    #              'lightslateblue', 'firebrick1', 'darkorange1', 
+    #              'blue4', 'yellow', 'thistle'),
+     mark.border = NA,
      edge.color = 'white',
      vertex.size = V(graph)$degree, 
      layout = lay,
-     edge.curved=0.25,
+     edge.curved = 0.1,
      )
 
 
+colors()
+
+c('royalblue1', 'pink3', 'orange4',
+'lightslateblue', 'firebrick1', 'darkorange1', 
+'blue4', 'yellow', 'thistle')
+
+
+unique(communityMulti)
+
+An Ecological Site Description is used to by Land Management Agencies to group similar areas, or motifs, across a landscape. In theory these ecological sites, due to their similar biophysical, and climatic properties have similar soils and vegetation, and should respond in similar manners to land management. A tradeoff exists between producing many narrow descriptions, and several broader descriptions. 
+
+In general the landscapes with ESD's completed are *somewhat* homogenous. However, a number of rare sites are still present. I argue that we can form, and as *needed* utilize broader conceptions of ESD's to supply context for rare sites across the landscape.
+
+Here we utilize an Assess, Inventory, and Monitor (AIM) sample design, which had ESD verification occur at 130 plots to serve as a focal group of ESD's. From this focal group we branch outwards along the ESD documents 'similar and associated sites' (this name... my own) sections to develop a framework for relating the ESD's using simple binary and undirected associations. 
